60cafbf06f969f1a018dd597c1bc12350bf45ed4
Slava <shpirkin95@mail.ru>
2022-10-06 22:10:57 +0300

    animation of blocks and the beginning of the layout of the contacts page

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,11 +1,1 @@
-$(document).ready(function(){
-    $('.coach__slider').slick({
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        dots: true,
-        autoplay: true,
-        autoplaySpeed: 1500,
-        prevArrow: '<button type="button" class="slick-prev"><img src="icons/arrow_left.png"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="icons/arrow_right.png"></button>',
-    });
-  });
+AOS.init();
