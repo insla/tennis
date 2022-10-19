c76976b1159f24d1f41b71f97d89457d7053414b
Slava <shpirkin95@mail.ru>
2022-10-19 22:15:24 +0300

    adaptive

--- dist/js/scriptJQuery.js
+++ dist/js/scriptJQuery.js
@@ -3,9 +3,17 @@ $(document).ready(function(){
         slidesToShow: 3,
         slidesToScroll: 1,
         dots: true,
-        autoplay: true,
+        autoplay: false,
         autoplaySpeed: 1500,
         prevArrow: '<button type="button" class="slick-prev"><img src="icons/arrow_left.png"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="icons/arrow_right.png"></button>',
+        responsive: [{
+            breakpoint: 767,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                arrows: false
+            }
+        }]
     });
 });
